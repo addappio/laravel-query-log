95d7d11180e7b99975054c872f4db40af22acec7
Dimitrios Savvopoulos <ds@dimsav.com>
2015-03-13 15:10:10 +0100

    Added service provider.

---
+++ src/QueryLogServiceProvider.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Addapp\QueryLog;
+
+use Illuminate\Support\ServiceProvider;
+use Event;
+use Log;
+use DateTime;
+
+
+class QueryLogServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $countQueries = 0;
+        Event::listen('illuminate.query', function ($query, $bindings) use (&$countQueries) {
+
+            $bindings = $this->formatBindingsForSqlInjection($bindings);
+            $query    = $this->insertBindingsIntoQuery($query, $bindings);
+
+            $countQueries++;
+            Log::info("--- Query $countQueries --- ".$query);
+        });
+    }
+
+    /**
+     * @param $bindings
+     *
+     * @return mixed
+     */
+    private function formatBindingsForSqlInjection($bindings)
+    {
+        foreach ($bindings as $i => $binding) {
+            if ($binding instanceof DateTime) {
+                $bindings[$i] = $binding->format('\'Y-m-d H:i:s\'');
+            } else {
+                if (is_string($binding)) {
+                    $bindings[$i] = "'$binding'";
+                }
+            }
+        }
+
+        return $bindings;
+    }
+
+    /**
+     * @param $query
+     * @param $bindings
+     *
+     * @return string
+     */
+    private function insertBindingsIntoQuery($query, $bindings)
+    {
+        $query = str_replace(array('%', '?'), array('%%', '%s'), $query);
+
+        return vsprintf($query, $bindings);
+    }
+}
